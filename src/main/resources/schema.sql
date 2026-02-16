f49dccebb5caded995f5c139c9d6baac478eafb6
HyeongSeok <hyeongseok1995@gmail.com>
2026-02-16 15:24:03 +0900

    refactor: H2 DB에서 PostgreSQL로 마이그레이션

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 create table if not exists product (
-    id bigint auto_increment primary key,
+    id bigserial  primary key,
     product_name VARCHAR(200) not null,
     survey_date DATE not null,
-    product_price DECIMAL(15, 2) not null,
+    product_price NUMERIC(15, 2) not null,
     store_name VARCHAR(200) not null,
     maker VARCHAR(200) not null,
     is_sale BOOLEAN,
